3a572195a5c3bcc2c80fd73ff60b19a6e82c2acc
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2023-08-20 15:20:37 -0700

    dev: update settings field name from url to link

--- src/Settings.ts
+++ src/Settings.ts
@@ -6,7 +6,7 @@ export interface ArticleScraperSettings {
   publishedDate: boolean;
   description: boolean;
   title: boolean;
-  url: boolean;
+  link: boolean;
 }
 
 export const DEFAULT_SETTINGS: Partial<ArticleScraperSettings> = {
@@ -14,7 +14,7 @@ export const DEFAULT_SETTINGS: Partial<ArticleScraperSettings> = {
   publishedDate: false,
   description: false,
   title: false,
-  url: false
+  link: false
 };
 
 export class ArticleScraperSettings extends PluginSettingTab {
@@ -33,56 +33,66 @@ export class ArticleScraperSettings extends PluginSettingTab {
     new Setting(containerEl)
       .setName("Author")
       .setDesc("Fetch the article's author.")
-      .addToggle((val) => val
-                 .onChange(async (val) => {
-                   console.log(val)
-                   this.plugin.settings.author = val
-                   await this.plugin.saveSettings()
-                 })
+      .addToggle((val) =>
+                val
+                  .setValue(this.plugin.settings.author)
+                  .onChange(async (val) => {
+                    console.log(val)
+                    this.plugin.settings.author = val
+                    await this.plugin.saveSettings()
+                  })
                 )
 
     new Setting(containerEl)
       .setName("Published Date")
       .setDesc("Fetch the article's published date.")
-      .addToggle((val) => val
-                 .onChange(async (val) => {
-                   console.log(val)
-                   this.plugin.settings.publishedDate = val
-                   await this.plugin.saveSettings()
-                 })
+      .addToggle((val) =>
+                val
+                  .setValue(this.plugin.settings.publishedDate)
+                  .onChange(async (val) => {
+                    console.log(val)
+                    this.plugin.settings.publishedDate = val
+                    await this.plugin.saveSettings()
+                  })
                 )
 
     new Setting(containerEl)
       .setName("Description")
       .setDesc("Fetch the article description.")
-      .addToggle((val) => val
-                 .onChange(async (val) => {
-                   console.log(val)
-                   this.plugin.settings.description = val
-                   await this.plugin.saveSettings()
-                 })
+      .addToggle((val) =>
+                val
+                  .setValue(this.plugin.settings.description)
+                  .onChange(async (val) => {
+                    console.log(val)
+                    this.plugin.settings.description = val
+                    await this.plugin.saveSettings()
+                  })
                 )
 
     new Setting(containerEl)
       .setName("Title")
       .setDesc("Fetch the article title.")
-      .addToggle((val) => val
-                 .onChange(async (val) => {
-                   console.log(val)
-                   this.plugin.settings.title = val
-                   await this.plugin.saveSettings()
-                 })
+      .addToggle((val) =>
+                val
+                  .setValue(this.plugin.settings.title)
+                  .onChange(async (val) => {
+                    console.log(val)
+                    this.plugin.settings.title = val
+                    await this.plugin.saveSettings()
+                  })
                 )
 
     new Setting(containerEl)
       .setName("Link")
       .setDesc("Fetch the article link.")
-      .addToggle((val) => val
-                 .onChange(async (val) => {
-                   console.log(val)
-                   this.plugin.settings.url = val
-                   await this.plugin.saveSettings()
-                 })
+      .addToggle((val) =>
+                val
+                  .setValue(this.plugin.settings.link)
+                  .onChange(async (val) => {
+                    console.log(val)
+                    this.plugin.settings.link = val
+                    await this.plugin.saveSettings()
+                  })
                 )
   }
 }
