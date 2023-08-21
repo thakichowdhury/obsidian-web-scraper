3b11a7a0ca55e5b05bd468ceb195063c5f0e8296
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2023-08-20 19:51:57 -0700

    clean: remove extraneous comments and code

--- src/App.ts
+++ src/App.ts
@@ -3,11 +3,6 @@ import { Plugin, request } from 'obsidian';
 import { ArticleScraperSettings, ArticleScraperSettingsType, DEFAULT_SETTINGS } from './Settings'
 import MetaScraper from './MetaScraper';
 
-type MetascraperInputType = {
-  html: string;
-  url: string;
-}
-
 export default class ArticleScraper extends Plugin {
   settings: ArticleScraperSettingsType;
 
@@ -29,28 +24,10 @@ export default class ArticleScraper extends Plugin {
   async fetchData(url: string) {
     const html = await request(url)
 
-    const metadata = await this._scraper({ html, url })
+    const scraper = new MetaScraper({ fields: this.settings }).scraper
+    const metadata = await scraper({ html, url })
     console.log(metadata)
+
     return metadata
   }
-
-  private _scraper({ html, url }: MetascraperInputType) {
-    const metaScraper = new MetaScraper({ fields: this.settings }).scraper
-    return metaScraper({ html, url })
-  }
-
-  // private _fieldScrapers() {
-  //   const fieldScrapers = {
-  //     author: msAuthor(),
-  //     publishedDate: msDate(),
-  //     description: msDescription(),
-  //     title: msTitle(),
-  //     link: msURL()
-  //   }
-    
-  //   // return Object.keys(fieldScrapers).filter((field) => this.settings[field as keyof ArticleScraperSettingsType])
-  //   return Object.keys(fieldScrapers)
-  //     .filter((field) => this.settings[field as keyof ArticleScraperSettingsType])
-  //     .map((field) => fieldScrapers[field as keyof typeof fieldScrapers])
-  // }
 }
