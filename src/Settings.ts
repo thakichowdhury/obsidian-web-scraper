a93d0f0d670fac4bd50a4c5f17ffaa8523415238
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2023-08-20 17:39:42 -0700

    fix: update ArticleScraperSettingsType fields

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,98 +1,73 @@
 import ArticleScraper from "./App";
 import { App, PluginSettingTab, Setting } from "obsidian";
 
-export interface ArticleScraperSettings {
+export interface ArticleScraperSettingsType {
   author: boolean;
   publishedDate: boolean;
-  description: boolean;
-  title: boolean;
-  link: boolean;
+  description: boolean,
+  title: boolean,
+  link: boolean,
+  image: boolean,
+  language: boolean,
+  logo: boolean,
+  publisher: boolean,
+  feed: boolean,
+  favicon: boolean,
 }
 
-export const DEFAULT_SETTINGS: Partial<ArticleScraperSettings> = {
+export const DEFAULT_SETTINGS: Partial<ArticleScraperSettingsType> = {
   author: false,
   publishedDate: false,
   description: false,
   title: false,
-  link: false
-};
+  link: false,
+  image: false,
+  language: false,
+  logo: false,
+  publisher: false,
+  feed: false,
+  favicon: false,
+}
+
 
 export class ArticleScraperSettings extends PluginSettingTab {
-  plugin: ArticleScraper;
+  plugin: ArticleScraper
+  _container: HTMLElement
 
   constructor(app: App, plugin: ArticleScraper) {
     super(app, plugin);
     this.plugin = plugin;
+    this._container = this.containerEl
+  }
+
+  private _createSettingsToggle(settingKey: keyof ArticleScraperSettingsType) {
+    const keyTitleCase = this._camelCaseToTitleCase(settingKey)
+
+    new Setting(this._container)
+      .setName(keyTitleCase)
+      .setDesc(`Fetch the article's ${keyTitleCase}.`)
+      .addToggle((val) =>
+        val
+          .setValue(this.plugin.settings[settingKey])
+          .onChange(async (val) => {
+            this.plugin.settings[settingKey] = val
+            await this.plugin.saveSettings()
+          })
+        )
+  }
+
+  private _camelCaseToTitleCase(str: string) {
+    return str
+      .split(/(?<!^)(?=[A-Z])/)
+      .map((word: string) => word[0].toUpperCase() + word.slice(1))
+      .join(' ')
   }
 
   display(): void {
-    let { containerEl } = this;
+    this._container.empty()
 
-    containerEl.empty();
-
-    new Setting(containerEl)
-      .setName("Author")
-      .setDesc("Fetch the article's author.")
-      .addToggle((val) =>
-                val
-                  .setValue(this.plugin.settings.author)
-                  .onChange(async (val) => {
-                    console.log(val)
-                    this.plugin.settings.author = val
-                    await this.plugin.saveSettings()
-                  })
-                )
-
-    new Setting(containerEl)
-      .setName("Published Date")
-      .setDesc("Fetch the article's published date.")
-      .addToggle((val) =>
-                val
-                  .setValue(this.plugin.settings.publishedDate)
-                  .onChange(async (val) => {
-                    console.log(val)
-                    this.plugin.settings.publishedDate = val
-                    await this.plugin.saveSettings()
-                  })
-                )
-
-    new Setting(containerEl)
-      .setName("Description")
-      .setDesc("Fetch the article description.")
-      .addToggle((val) =>
-                val
-                  .setValue(this.plugin.settings.description)
-                  .onChange(async (val) => {
-                    console.log(val)
-                    this.plugin.settings.description = val
-                    await this.plugin.saveSettings()
-                  })
-                )
-
-    new Setting(containerEl)
-      .setName("Title")
-      .setDesc("Fetch the article title.")
-      .addToggle((val) =>
-                val
-                  .setValue(this.plugin.settings.title)
-                  .onChange(async (val) => {
-                    console.log(val)
-                    this.plugin.settings.title = val
-                    await this.plugin.saveSettings()
-                  })
-                )
-
-    new Setting(containerEl)
-      .setName("Link")
-      .setDesc("Fetch the article link.")
-      .addToggle((val) =>
-                val
-                  .setValue(this.plugin.settings.link)
-                  .onChange(async (val) => {
-                    console.log(val)
-                    this.plugin.settings.link = val
-                    await this.plugin.saveSettings()
-                  })
-                )
+    // create toggle setting for each field
+    Object.keys(this.plugin.settings)
+      .forEach((settingKey: keyof ArticleScraperSettingsType) => this._createSettingsToggle(settingKey))
   }
 }
